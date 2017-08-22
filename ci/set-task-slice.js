26ed406b998b0914b00b833f68cdb78cc650173d
Eric Sciple <ersciple@microsoft.com>
2017-08-22 17:05:33 -0400

    .

--- ci/set-task-slice.js
+++ ci/set-task-slice.js
@@ -3,7 +3,7 @@ var path = require('path');
 var makeOptionsPath = path.join(__dirname, '..', 'make-options.json');
 var makeOptions = JSON.parse(fs.readFileSync(makeOptionsPath).toString())
 var tasks = makeOptions.tasks.filter(function (val, index) {
-    return i % parseInt(process.env.SYSTEM_TOTALJOBSINPHASE) == parseInt(process.env.SYSTEM_JOBPOSITIONINPHASE) - 1;
+    return index % parseInt(process.env.SYSTEM_TOTALJOBSINPHASE) == parseInt(process.env.SYSTEM_JOBPOSITIONINPHASE) - 1;
 });
 
 console.log('##vso[task.setVariable variable=task_pattern]@(' + tasks.join('|') + ')');
